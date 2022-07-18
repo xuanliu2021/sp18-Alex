36610679b43f2d4114ab8863350c3b4fa479df66
Alex Liu <xl114@rice.edu>
2022-07-17 23:45:34 -0500

    Add Lab1 LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,15 +6,10 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
-    public static Boolean isLeapYear(int year){
-        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
-            return true;
-        }
-        else{
-            return false;
-        }
+    public static boolean isLeapYear(int year){
+        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
     }
-    
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
