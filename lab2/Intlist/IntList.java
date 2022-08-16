1da2a21b3c6348120362faca5b1a3ee00908bb13
Alex Liu <xl114@rice.edu>
2022-08-16 11:20:39 -0500

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -100,25 +100,25 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if (A == null) {
-            return B;
-        }
-
-        IntList res = new IntList(A.first, null);
-        IntList ptr = res;
-        A = A.rest;
-        while (A != null) {
-            ptr.rest = new IntList(A.first, null);
-            A = A.rest;
-            ptr = ptr.rest;
-        }
-        ptr.rest = B;
-        return res;
 //        if (A == null) {
 //            return B;
 //        }
 //
-//        return new IntList(A.first, catenate(A.rest, B));
+//        IntList res = new IntList(A.first, null);
+//        IntList ptr = res;
+//        A = A.rest;
+//        while (A != null) {
+//            ptr.rest = new IntList(A.first, null);
+//            A = A.rest;
+//            ptr = ptr.rest;
+//        }
+//        ptr.rest = B;
+//        return res;
+        if (A == null) {
+            return B;
+        }
+
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
